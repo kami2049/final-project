3c4b74a7be692e9b5c7d0d5ac0a01f0e062edd11
kamilah hamilton <hamilton8186@aol.com>
2017-10-22 23:33:10 -0400

    added routes to controller action

--- index.js
+++ index.js
@@ -1,23 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-// const jsonfile = require('jsonfile');
-const fs = require('fs');
+const jsonfile = require('jsonfile');
 const app = express();
 
 app.set('view engine', 'ejs');
 
-// jsonfile.readFile('/models/resource.json',  function(err, data) {
-//   console.log(data)
-// });
+const resourceController = require('./controller/resource');
 
-fs.readFile('./models/resource.json', 'utf8', function(err, data) {
-	if (err) throw err;
-	console.log(data);
-});
-
-app.get('/', function (req, res) {
-  res.render('home')
-});
+app.get('/', resourceController.get);
+app.get('/home', resourceController.get);
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
